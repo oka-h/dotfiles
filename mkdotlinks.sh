db6279fc1300a1f204e663014892abe0a2896a2f
0x841 <float.weasel@gmail.com>
2017-03-25 03:43:01 +0900

    [fix]

--- mkdotlinks.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-ln -s ~/vimsettings/vimrc ~/.vimrc
-# ln -s ~/vimsettings/vimrc ~/.vim/init.vim
-# ln -s ~/vimsettings/gvimrc ~/.gvimrc
-# ln -s ~/vimsettings/gvimrc ~/.vim/ginit.vim
-
