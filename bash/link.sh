707cacab5ab5ba066bb46b6e8eba3c417178c6cb
0x841 <float.weasel@gmail.com>
2017-12-12 00:39:00 +0900

    [update] Refactored and make zsh start faster

--- bash/link.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-if type bash > /dev/null 2>&1; then
-  bashrc=$(cd $(dirname $0) && pwd)/.bashrc
-  ln -s $bashrc ~/.bashrc
-fi
-
--- zsh/link.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-if type zsh > /dev/null 2>&1; then
-  zshenv=$(cd $(dirname $0) && pwd)/.zshenv
-  ln -s $zshenv ~/.zshenv
-fi
-
